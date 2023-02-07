5aafdbef2ad2fb02d084ee73cb9dc55489ac122c
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-07 22:52:28 +0530

    [Ritesh]add :Winning Position At Point 100

--- SnakeAndLadderGame/GamePlay.cs
+++ SnakeAndLadderGame/GamePlay.cs
@@ -9,7 +9,8 @@ namespace SnakeAndLadderGame
     public class GamePlay
     { 
         int Player_Position = 0;
-        const int No_Play = 0, Ladder = 1, Snake = 2;  
+        const int No_Play = 0, Ladder = 1, Snake = 2;
+        int winning_position = 100;
         Random random = new Random();   
     
         public void Game()
@@ -20,31 +21,49 @@ namespace SnakeAndLadderGame
         public int DieRoll()
         {
             //The Player Roll The Die To Get A Number Between 1 to 6 use Random
-            Random random = new Random();
+           
             int DiceRoll = random.Next(1,7);
             Console.WriteLine("The Player Get Number {0} on DiceRoll" , DiceRoll);
             return DiceRoll;
         }
         public void CheckOption()
         {
-            int option = random.Next(3);
-            switch(option)
+            string type = "";
+            while (Player_Position < winning_position)
             {
-                case No_Play:
-                    Player_Position = 0;
-                    break;
-                case Ladder:
-                    Player_Position += DieRoll();
-                    break;
-                case Snake:
-                    Player_Position -= DieRoll();
-                       break;
-                default:
-                    Console.WriteLine("Invalid Selection");
-                    break;
+                int option = random.Next(3);
+                switch (option)
+                {
+                    case No_Play:
+                        type = "No_Player";
+                        break;
+                    case Ladder:
+                        Player_Position += DieRoll();
+                        type = "Ladder";
+                        if (Player_Position > 100)
+                        {
+                            Console.WriteLine("Get second chance To Win");
+                            break;
+                        }
+                        break;
+                    case Snake:
+                        Player_Position -= DieRoll();
+                        type = "Snake";
+                        if (Player_Position < 0)
+                        {
+                            Player_Position = 0;
+                        }
+                            break;
+                    default:
+                        Console.WriteLine("Invalid Selection");
+                        break;
 
+                }
+                Console.WriteLine("Type ="+type);
+                Console.WriteLine("The Player Is Current Position = "+Player_Position);
+                Console.WriteLine("___________________");
             }
-            Console.WriteLine("The Player Is Current Position = "+Player_Position);
+            Console.WriteLine("Game Over ! Player Is At Winning position {0} " +Player_Position);
         }
     }
 }
